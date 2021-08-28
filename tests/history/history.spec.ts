8af01b45f84142931ce8a400fb3973343f133179
Adam Bradley <adamdbradley@users.noreply.github.com>
2021-08-28 13:23:24 -0500

    patch history.pushState()

--- tests/history/history.spec.ts
+++ tests/history/history.spec.ts
@@ -11,13 +11,21 @@ test('history', async ({ page }) => {
   const testPushStateObj = page.locator('#testPushStateObj');
   await expect(testPushStateObj).toHaveText(JSON.stringify({ mph: 88 }));
 
-  const buttonPushState121 = page.locator('#buttonPushState121');
-  await buttonPushState121.click();
+  const testReplaceState = page.locator('#testReplaceState');
+  const buttonReplaceState121 = page.locator('#buttonReplaceState121');
+  await buttonReplaceState121.click();
   const expected121 = new URL('/history/1.21gw', page.url());
   await expect(page).toHaveURL(expected121.href);
+  await expect(testReplaceState).toHaveText('1955');
 
   const buttonReplaceState = page.locator('#buttonReplaceState');
   await buttonReplaceState.click();
   const expectedHistory = new URL('/history/', page.url());
   await expect(page).toHaveURL(expectedHistory.href);
+  await expect(testReplaceState).toHaveText('1985');
+
+  const testPatchPushState = page.locator('#testPatchPushState');
+  const buttonPatchPushState = page.locator('#buttonPatchPushState');
+  await buttonPatchPushState.click();
+  await expect(testPatchPushState).toHaveText('Hill Valley');
 });
