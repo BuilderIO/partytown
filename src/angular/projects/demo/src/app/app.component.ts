0546c809765cac6324df07322fd1f0ea8fffd6f8
jaybell <jaycooperbell@gmail.com>
2021-09-26 08:26:12 -0700

    refactor(angular): remove excess files from ng workspace

--- src/angular/projects/demo/src/app/app.component.ts
+++ src/angular/projects/demo/src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: 'Party!',
+  styles: [''],
 })
-export class AppComponent {
-  title = 'demo';
-}
+export class AppComponent {}
