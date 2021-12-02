612a6534aeb2a6eb7b807506c2113152c858f8b8
Adam Bradley <adamdbradley@users.noreply.github.com>
2021-12-01 23:45:30 -0600

    chore: update js banner

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -122,7 +122,7 @@ export function onwarn(warning: RollupWarning) {
 }
 
 export function getJsBanner(opts: BuildOptions, jsCode: string) {
-  return `/* Partytown ${opts.packageJson.version} - MIT https://builder.io */\n${jsCode}`;
+  return `/* Partytown ${opts.packageJson.version} - MIT builder.io */\n${jsCode}`;
 }
 
 export function jsBannerPlugin(opts: BuildOptions): Plugin {
