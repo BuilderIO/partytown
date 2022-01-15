fa701dae2728bf8a258901051405b9b5fb21465d
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-01-15 11:31:31 -0600

    test: webgl canvas

--- tests/platform/canvas/canvas.spec.ts
+++ tests/platform/canvas/canvas.spec.ts
@@ -5,9 +5,21 @@ test('canvas', async ({ page }) => {
 
   await page.waitForSelector('.completed');
 
-  const testGetContext2d = page.locator('#testGetContext2d');
-  await expect(testGetContext2d).toHaveText('#000000');
+  const testGetContext2dCanvas = page.locator('#testGetContext2dCanvas');
+  await expect(testGetContext2dCanvas).toHaveText('CANVAS');
 
   const testMeasureText = page.locator('#testMeasureText');
   await expect(testMeasureText).not.toHaveText('');
+
+  const testFillStyle = page.locator('#testFillStyle');
+  await expect(testFillStyle).toHaveText('blue');
+
+  const testCreateRadialGradient = page.locator('#testCreateRadialGradient');
+  await expect(testCreateRadialGradient).toHaveText('CanvasGradient');
+
+  const testGetContextWebGLCanvas = page.locator('#testGetContextWebGLCanvas');
+  await expect(testGetContextWebGLCanvas).toHaveText('CANVAS');
+
+  const testWebGLRenderInfo = page.locator('#testWebGLRenderInfo');
+  await expect(testWebGLRenderInfo).not.toHaveText('');
 });
