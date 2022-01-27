b07e04773858651761a06d39154965daefcabfa4
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-01-27 06:02:30 -0600

    test: update adobe launch test

--- tests/integrations/adobe-launch/adobe-launch.spec.ts
+++ tests/integrations/adobe-launch/adobe-launch.spec.ts
@@ -2,5 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('adobe-launch', async ({ page }) => {
   await page.goto('/integrations/adobe-launch/');
+
+  const btnTrack = page.locator('#track');
+  await btnTrack.click();
+
   await page.waitForSelector('.completed');
+
+  const result = page.locator('#result');
+  await expect(result).toHaveText('tracked');
 });
