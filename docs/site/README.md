dcf79e6b003e13c8cf4249c30df1b2e93622d7a0
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-02-02 17:38:21 -0600

    ⛑

---
+++ docs/site/README.md
@@ -0,0 +1,8 @@
+# Party Docs Site
+
+| Command           | Action                                       |
+| :---------------- | :------------------------------------------- |
+| `npm install`     | Installs dependencies                        |
+| `npm run dev`     | Starts local dev server at `localhost:3000`  |
+| `npm run build`   | Build your production site to `./dist/`      |
+| `npm run preview` | Preview your build locally, before deploying |
---
+++ docs/site/src/config.ts
@@ -0,0 +1,18 @@
+export const SITE = {
+  title: "Partytown",
+  description: "Run scripts from a web worker.",
+  defaultLanguage: "en_US",
+};
+
+export const OPEN_GRAPH = {
+  image: {
+    src: "https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true",
+    alt:
+      "astro logo on a starry expanse of space," +
+      " with a purple saturn-like planet floating in the right foreground",
+  },
+  twitter: "Builder.io",
+};
+
+// Uncomment this to add an "Edit this page" button to every page of documentation.
+export const GITHUB_EDIT_URL = `https://github.com/BuilderIO/partytown/edit/docs/docs`;
