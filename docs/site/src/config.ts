209b69aec5f09e78a89e0f27ce9c558c86f3c3a0
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-02-02 22:18:44 -0600

    🗻

--- docs/site/src/config.ts
+++ docs/site/src/config.ts
@@ -1,17 +1,7 @@
 export const SITE = {
-  title: "Partytown",
-  description: "Run scripts from a web worker.",
-  defaultLanguage: "en_US",
-};
-
-export const OPEN_GRAPH = {
-  image: {
-    src: "https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true",
-    alt:
-      "astro logo on a starry expanse of space," +
-      " with a purple saturn-like planet floating in the right foreground",
-  },
-  twitter: "Builder.io",
+  title: 'Partytown',
+  description: 'Run scripts from a web worker.',
+  defaultLanguage: 'en_US',
 };
 
 // Uncomment this to add an "Edit this page" button to every page of documentation.
