cfb4a2366778b9ba3639a0592193bd819951a65a
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-02-27 11:24:06 -0800

    test: forms

--- tests/platform/document/document.spec.ts
+++ tests/platform/document/document.spec.ts
@@ -73,6 +73,9 @@ test('document', async ({ page }) => {
   const testDocumentChildNodes = page.locator('#testDocumentChildNodes');
   await expect(testDocumentChildNodes).toHaveText('2 [html(10), HTML(1)]');
 
+  const testDocumentForms = page.locator('#testDocumentForms');
+  await expect(testDocumentForms).toHaveText('0');
+
   const testDocType = page.locator('#testDocType');
   await expect(testDocType).toHaveText('10 html html false');
 
