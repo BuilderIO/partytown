cc27402131ab52f4d141b089be55cccf27f9e6f2
Miguel Montalvo <miguel.montalvo@barrelny.com>
2022-03-02 19:33:57 -0800

    test: form.method
    start tests for HTMLFormElement

---
+++ tests/platform/form/form.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('form', async ({ page }) => {
+  await page.goto('/tests/platform/form/');
+
+  await page.waitForSelector('.completed');
+
+  const testFormMethod = page.locator('#testFormMethod');
+  await expect(testFormMethod).toHaveText('get');
+});
