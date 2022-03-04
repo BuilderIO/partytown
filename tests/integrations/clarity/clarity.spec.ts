d5c5d6c1aad8576d901d948d957c8b7f73cff935
Sanyam Kamat <sanyamkamat@gmail.com>
2022-03-04 16:59:58 +0530

    wip Clarity app integration bug

---
+++ tests/integrations/clarity/clarity.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('gtm', async ({ page }) => {
+  // await page.goto('/tests/integrations/gtm/');
+  //
+  // await page.waitForSelector('.completed');
+  //
+  // const buttonDataLayerPush = page.locator('#buttonDataLayerPush');
+  // await buttonDataLayerPush.click();
+  //
+  // const testDataLayer = page.locator('#testDataLayer');
+  // await expect(testDataLayer).toHaveText('pushed');
+
+
+});
