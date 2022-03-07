b3125e616406a95d7dc97597e32ab5f49c4a5c25
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-03-06 20:35:59 -0600

    test: jquery

---
+++ tests/integrations/jquery/jquery.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('jquery', async ({ page }) => {
+  // await page.goto('/tests/integrations/jquery/');
+  // await page.waitForSelector('.completed');
+  // const buttonDataLayerPush = page.locator('#buttonDataLayerPush');
+  // await buttonDataLayerPush.click();
+  // const testDataLayer = page.locator('#testDataLayer');
+  // await expect(testDataLayer).toHaveText('pushed');
+});
