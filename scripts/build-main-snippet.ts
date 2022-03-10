922087a0d123fe1d18a27ffa7ede2f04f8198f7e
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-03-09 20:38:59 -0600

    chore: do not run fallback in local build

--- scripts/build-main-snippet.ts
+++ scripts/build-main-snippet.ts
@@ -1,14 +1,28 @@
 import { BuildOptions, fileSize, jsBannerPlugin, versionPlugin } from './utils';
 import { join } from 'path';
 import { minifyPlugin } from './minify';
-import type { OutputOptions, RollupOptions } from 'rollup';
+import type { OutputOptions, Plugin, RollupOptions } from 'rollup';
 
 export function buildMainSnippet(opts: BuildOptions): RollupOptions {
   const partytownDebug: OutputOptions = {
     file: join(opts.distLibDebugDir, 'partytown.js'),
     format: 'es',
     exports: 'none',
-    plugins: [versionPlugin(opts), ...minifyPlugin(opts, true)],
+    plugins: [
+      versionPlugin(opts),
+      ...minifyPlugin(opts, true),
+      {
+        name: 'fallbackTimeout',
+        generateBundle(opts, bundle) {
+          for (const f in bundle) {
+            const b = bundle[f];
+            if (b.type === 'chunk') {
+              b.code = b.code.replace(`"use strict";`, ``).trim();
+            }
+          }
+        },
+      },
+    ],
   };
 
   const partytownMin: OutputOptions = {
@@ -19,7 +33,7 @@ export function buildMainSnippet(opts: BuildOptions): RollupOptions {
       ...minifyPlugin(opts, false),
       {
         name: 'snippetClosure',
-        generateBundle(opts, bundle) {
+        generateBundle(_, bundle) {
           for (const f in bundle) {
             const b = bundle[f];
             if (b.type === 'chunk') {
@@ -28,8 +42,8 @@ export function buildMainSnippet(opts: BuildOptions): RollupOptions {
           }
         },
       },
-      fileSize(),
       versionPlugin(opts),
+      fileSize(),
     ],
   };
 
@@ -41,6 +55,23 @@ export function buildMainSnippet(opts: BuildOptions): RollupOptions {
   return {
     input: join(opts.tscLibDir, 'main', 'snippet-entry.js'),
     output,
-    plugins: [jsBannerPlugin(opts)],
+    plugins: [jsBannerPlugin(opts), fallbackTimeoutMsPlugin(opts)],
+  };
+}
+
+function fallbackTimeoutMsPlugin(opts: BuildOptions): Plugin {
+  // local build shouldn't run the fallback
+  // a production build for release should set the fallback of 10 seconds
+  const fallbackMs = opts.isReleaseBuild ? '1e4' /* 10000 */ : '999999999';
+  return {
+    name: 'fallbackTimeoutMsPlugin',
+    async generateBundle(_, bundles) {
+      for (const f in bundles) {
+        const bundle = bundles[f];
+        if (bundle.type === 'chunk') {
+          bundle.code = bundle.code.replace('9999', fallbackMs);
+        }
+      }
+    },
   };
 }
