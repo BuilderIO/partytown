9c1762bc6dfaca7da50e135a02ef41c7d57ec1c1
Sanyam Kamat <sanyamkamat@gmail.com>
2022-05-03 20:42:42 +0530

    feat: allow Partytown to access window objects from main thread (#166)

---
+++ tests/integrations/main-window-accessors/main-window-accessors.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test.only('integration window accessor', async ({ page }) => {
+  await page.goto('/tests/integrations/main-window-accessors/');
+  await page.waitForSelector('.completed');
+
+  const element = page.locator('#testWindowAccessor');
+  await expect(element).toHaveText('42');
+
+  const child_element = page.locator('#testChildWindowAccessor');
+  await expect(child_element).toHaveText('stringValue');
+});
