3e0955de5959dc6b662be70e492339ad545a3068
Slawek Kolodziej <hfrntt@gmail.com>
2022-05-06 00:06:33 +0200

    refactor: remove popstate/hashchange event handlers from worker-location

--- src/lib/web-worker/worker-location.ts
+++ src/lib/web-worker/worker-location.ts
@@ -3,14 +3,6 @@ import { HistoryChangePrevent } from "./worker-constants";
 
 const isObject = (value: any) => typeof value === 'object' && value !== null;
 
-type PopStateEvent = Event & {
-  state?: LocationUpdateData['state'];
-};
-type HashChangeEvent = Event & {
-  newURL?: LocationUpdateData['newUrl'],
-  oldURL?: LocationUpdateData['oldUrl']
-};
-
 export function forwardLocationChange($winId$: number, env: WebWorkerEnvironment, data: LocationUpdateData) {
   const history = env.$window$.history;
 
@@ -26,23 +18,6 @@ export function forwardLocationChange($winId$: number, env: WebWorkerEnvironment
         history.replaceState({...data.state, [HistoryChangePrevent]: true}, '', data.newUrl)
       }
       break;
-    }
-    case LocationUpdateType.PopState: {
-      // FIXME: This WILL cause infinite loop for now
-      // debugger;
-      const event: PopStateEvent = new Event('popstate');
-      event.state = data.state;
-      env.$window$.dispatchEvent(event);
-      break;
-    }
-    case LocationUpdateType.HashChange: {
-      // FIXME: This WILL cause infinite loop for now
-      // debugger;
-      const event: HashChangeEvent = new Event('hashchange', )
-      event.newURL = data.newUrl;
-      event.oldURL = data.oldUrl;
-      env.$window$.dispatchEvent(event);
-      break
     }
   }
 }
