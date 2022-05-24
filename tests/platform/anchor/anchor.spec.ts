e4b12bc2c195530e5322f402b2165bc783f3b0bf
Slawek Kolodziej <hfrntt@gmail.com>
2022-05-24 16:39:39 +0200

    fix: anchor property setter (#184)

--- tests/platform/anchor/anchor.spec.ts
+++ tests/platform/anchor/anchor.spec.ts
@@ -30,4 +30,22 @@ test('anchor', async ({ page }) => {
   await page.waitForSelector('.testSetHref');
   const testSetHref = page.locator('#testSetHref');
   await expect(testSetHref).toHaveText('/pathname');
+
+  await page.waitForSelector('.testSetHref2');
+  const testSetHref2 = page.locator('#testSetHref2');
+  const desiredLocalUrl = new URL(page.url());
+  desiredLocalUrl.pathname = '/local-pathname'
+  await expect(testSetHref2).toHaveText(desiredLocalUrl.toString());
+
+  await page.waitForSelector('.testGetSearch');
+  const testGetSearch = page.locator('#testGetSearch');
+  await expect(testGetSearch).toHaveText('?a=42&b=23');
+  const testGetSearchHref = page.locator('#testGetSearchHref');
+  await expect(testGetSearchHref).toHaveText('https://builder.io/?a=42&b=23');
+
+  await page.waitForSelector('.testSetSearch');
+  const testSetSearch = page.locator('#testSetSearch');
+  await expect(testSetSearch).toHaveText('?x=1&y=2');
+  const testSetSearchHref = page.locator('#testSetSearchHref');
+  await expect(testSetSearchHref).toHaveText('https://builder.io/?x=1&y=2');
 });
