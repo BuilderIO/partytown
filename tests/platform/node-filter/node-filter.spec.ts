d495ca97769e98d011f49e84ddbbbdef78de1a22
Miško Hevery <misko@hevery.com>
2023-08-02 10:51:15 +0200

    Revert "add test cases for NodeFilter and TreeWalker (#252)"
    This reverts commit e5fbbac472d4b262fc113e7930a0d74f3386ada7.

--- tests/platform/node-filter/node-filter.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('NodeFilter', async ({ page }) => {
-  await page.goto('/tests/platform/node-filter/');
-  await page.waitForSelector('#nodeFilterProps');
-  const resultDiv = page.locator('#nodeFilterProps');
-  await expect(resultDiv).toContainText('FILTER_ACCEPT');
-});
-
--- tests/platform/tree-walker/tree-walker.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('TreeWalker', async ({ page }) => {
-  await page.goto('/tests/platform/tree-walker/');
-  await page.waitForSelector('#walkerResult');
-  const resultDiv = page.locator('#walkerResult');
-  await expect(resultDiv).toHaveText('BODY H1 DIV SCRIPT');
-});
-
