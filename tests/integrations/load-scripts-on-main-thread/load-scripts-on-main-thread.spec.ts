97681d7b9601bdfc17046086312b5d6e24b185a6
Dylan Moore <60218243+dymoo@users.noreply.github.com>
2023-12-26 18:34:58 +0000

    regex-test-script.js test

--- tests/integrations/load-scripts-on-main-thread/load-scripts-on-main-thread.spec.ts
+++ tests/integrations/load-scripts-on-main-thread/load-scripts-on-main-thread.spec.ts
@@ -7,6 +7,9 @@ test('integration window accessor', async ({ page }) => {
   const scriptElement = page.locator('#testScript');
   await expect(scriptElement).toHaveAttribute('type', 'text/javascript')
 
+  const regexScriptElement = page.locator('#regexTestScript');
+  await expect(regexScriptElement).toHaveAttribute('type', 'text/javascript')
+
   await page.waitForSelector('.testInlineScript');
   const testInlineScript = page.locator('#testInlineScript');
   await expect(testInlineScript).toHaveText('12');
