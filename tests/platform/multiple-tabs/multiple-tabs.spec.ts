af80d747eed88d19c773f5296b205f938083d3cd
andrey <mr.vladetsky@yandex.ru>
2024-03-22 14:42:40 +0300

    Test Fix

--- tests/platform/multiple-tabs/multiple-tabs.spec.ts
+++ tests/platform/multiple-tabs/multiple-tabs.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('multiple tabs', async ({ page }) => {
+test('multiple tabs', async ({ page, context }) => {
   await page.goto('/tests/platform/multiple-tabs/');
 
   await page.waitForSelector('.completed');
@@ -9,9 +9,15 @@ test('multiple tabs', async ({ page }) => {
   await expect(testNewTabs).toHaveText('0');
 
   const linkNewTab = page.locator('#openNewTab');
+  const newPagePromise = context.waitForEvent('page');
   await linkNewTab.click();
+  const newPage = await newPagePromise;
+  await newPage.waitForSelector('.completed');
   await expect(testNewTabs).toHaveText('1');
 
+  const newPagePromise2 = context.waitForEvent('page');
   await linkNewTab.click();
+  const newPage2 = await newPagePromise2;
+  await newPage2.waitForSelector('.completed');
   await expect(testNewTabs).toHaveText('2');
 });
